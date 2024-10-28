1a8e98762e1cd3ba1a0423b1ed6d0989d44612a6
nas11ai <kairupannasai@gmail.com>
2024-10-28 10:04:42 +0800

    feat: init peer-rektor

---
+++ hyperledger-fabric/peer-rektor/join-channel.sh
@@ -0,0 +1,22 @@
+# Set environment variables
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="RektorMSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/crypto-config/peerOrganizations/rektor.itk.com/peers/peer0.rektor.itk.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/crypto-config/peerOrganizations/rektor.itk.com/users/Admin@rektor.itk.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+
+# Set orderer TLS certificate
+export ORDERER_CA=${PWD}/../orderer/crypto-config/ordererOrganizations/itk.com/orderers/orderer0.itk.com/tls/ca.crt
+
+# Fetch genesis block
+peer channel fetch 0 ./artifacts/ijazah-channel.block -o localhost:7050 \
+--channelID ijazah-channel \
+--ordererTLSHostnameOverride orderer0.itk.com \
+--tls \
+--cafile $ORDERER_CA
+
+# Join channel
+peer channel join -b ./artifacts/ijazah-channel.block
+
+# List channels yang sudah di-join
+peer channel list
