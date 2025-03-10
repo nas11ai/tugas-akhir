47729a6ed3d8505556437b9a3c3e7641ec2ee116
nas11ai <kairupannasai@gmail.com>
2025-03-10 11:38:02 +0800

    feat: init result page

--- vuetify-project/typed-router.d.ts
+++ vuetify-project/typed-router.d.ts
@@ -19,5 +19,6 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/Search-Result': RouteRecordInfo<'/Search-Result', '/Search-Result', Record<never, never>, Record<never, never>>,
   }
 }
