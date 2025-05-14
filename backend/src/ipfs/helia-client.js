ec176d88afda5fd567b5d63d86f89bcc1e97f0cd
nas11ai <kairupannasai@gmail.com>
2025-05-14 20:39:06 +0800

    feat: init ipfs connection with helia

--- backend/src/ipfs/helia-client.js
+++ backend/src/ipfs/helia-client.js
@@ -1,7 +1,35 @@
-import { createHelia } from 'helia';
-import { unixfs } from '@helia/unixfs';
+import { createHelia } from 'helia'
+import { createLibp2p } from 'libp2p'
+import { webSockets } from '@libp2p/websockets'
+import { noise } from '@chainsafe/libp2p-noise'
+import { mplex } from '@libp2p/mplex'
+import { kadDHT } from '@libp2p/kad-dht'
+import { bootstrap } from '@libp2p/bootstrap'
+import { createFsBlockstore } from 'blockstore-fs'
+import fs from 'fs'
+import path from 'path'
 
-const helia = await createHelia();
-const fsStore = unixfs(helia);
+const bootstrapNode = process.env.BOOTSTRAP_PEER_IP
 
-export { helia, fsStore };
+const dir = path.resolve('./ipfs-data')
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir, { recursive: true })
+}
+
+const blockstore = await createFsBlockstore('./ipfs-data') // optional: simpan data di disk
+
+const libp2p = await createLibp2p({
+  transports: [webSockets()],
+  connectionEncryption: [noise()],
+  streamMuxers: [mplex()],
+  dht: kadDHT(),
+  peerDiscovery: [
+    bootstrap({
+      list: [bootstrapNode]
+    })
+  ]
+})
+
+export const helia = await createHelia({ libp2p, blockstore })
+
+export const fsStore = helia.blockstore
