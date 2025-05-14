71b071f9b071104d2e23e4f0831a7683be790832
nas11ai <kairupannasai@gmail.com>
2025-05-14 21:02:22 +0800

    fix: blockstore-fs setup in helia client

--- backend/src/ipfs/helia-client.js
+++ backend/src/ipfs/helia-client.js
@@ -5,18 +5,23 @@ import { noise } from '@chainsafe/libp2p-noise'
 import { mplex } from '@libp2p/mplex'
 import { kadDHT } from '@libp2p/kad-dht'
 import { bootstrap } from '@libp2p/bootstrap'
-import { createFsBlockstore } from 'blockstore-fs'
+import { FsBlockstore } from 'blockstore-fs'
 import fs from 'fs'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const bootstrapNode = process.env.BOOTSTRAP_PEER_IP
+const dir = path.resolve(__dirname, 'ipfs-data')
 
-const dir = path.resolve('./ipfs-data')
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir, { recursive: true })
 }
 
-const blockstore = await createFsBlockstore('./ipfs-data') // optional: simpan data di disk
+const blockstore = new FsBlockstore(dir)
+await blockstore.open()
 
 const libp2p = await createLibp2p({
   transports: [webSockets()],
@@ -31,5 +36,4 @@ const libp2p = await createLibp2p({
 })
 
 export const helia = await createHelia({ libp2p, blockstore })
-
 export const fsStore = helia.blockstore
