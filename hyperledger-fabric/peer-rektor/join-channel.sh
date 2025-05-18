6a227d5cdb37cd9f3375e075bcf261a502a2c1eb
nas11ai <kairupannasai@gmail.com>
2025-05-19 00:37:36 +0800

    feat: init core.yaml

--- hyperledger-fabric/peer-rektor/join-channel.sh
+++ hyperledger-fabric/peer-rektor/join-channel.sh
@@ -4,9 +4,13 @@ export CORE_PEER_LOCALMSPID="RektorMSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/crypto-config/peerOrganizations/rektor.itk.com/peers/peer0.rektor.itk.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=${PWD}/crypto-config/peerOrganizations/rektor.itk.com/users/Admin@rektor.itk.com/msp
 export CORE_PEER_ADDRESS=localhost:7051
+export CORE_PEER_BCCSP_DEFAULT=SW
+export CORE_PEER_BCCSP_SW_HASH=SHA2
+export CORE_PEER_BCCSP_SW_SECURITY=256
+export CORE_PEER_BCCSP_SW_FILEKEYSTORE_KEYSTORE=${PWD}/crypto-config/peerOrganizations/rektor.itk.com/users/Admin@rektor.itk.com/msp/keystore
 
 # Join channel
-peer channel join -b ./artifacts/ijazah-channel.block
+peer channel join -b ./artifacts/ijazah-channel.block --tls --cafile ${PWD}/crypto-config/ordererOrganizations/itk.com/tlsca/tlsca.itk.com-cert.pem
 
 # List channels yang sudah di-join
 peer channel list
