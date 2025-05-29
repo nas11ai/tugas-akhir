8f7dfa1709982411eaa7abd44aef21937e4ea1c7
nas11ai <kairupannasai@gmail.com>
2025-05-29 17:17:22 +0800

    feat: update view

--- vuetify-project/src/pages/ijazah/[id].vue
+++ vuetify-project/src/pages/ijazah/[id].vue
@@ -25,86 +25,24 @@
 
       <!-- Main Content -->
       <div v-if="!loading && !error && ijazah">
-        <!-- Certificate Display -->
-        <v-row class="">
-          <v-col cols="12">
-            <v-sheet class="pa-4" outlined>
-              <!-- Tombol Kembali -->
-              <v-btn color="blue" outlined @click="goBack" class="mb-4">
-                <v-icon left>mdi-arrow-left</v-icon>
-                Kembali
-              </v-btn>
-              <v-card-title class="text-center">
-                <h3>Sertifikat Ijazah</h3>
-              </v-card-title>
-
-              <!-- Certificate Image -->
-              <div v-if="certificateUrl" class="text-center">
-                <v-img
-                  :src="certificateUrl"
-                  :alt="`Sertifikat ${ijazah.nama}`"
-                  aspect-ratio="16/9"
-                  cover
-                  class="w-100"
-                  max-height="500"
-                  @error="onImageError"
-                >
-                  <template v-slot:placeholder>
-                    <v-row
-                      class="fill-height ma-0"
-                      align="center"
-                      justify="center"
-                    >
-                      <v-progress-circular
-                        indeterminate
-                        color="grey lighten-5"
-                      ></v-progress-circular>
-                    </v-row>
-                  </template>
-                </v-img>
-
-                <v-btn
-                  color="primary"
-                  class="mt-3"
-                  @click="downloadCertificate"
-                >
-                  <v-icon left>mdi-download</v-icon>
-                  Download Sertifikat
-                </v-btn>
-              </div>
-
-              <!-- Placeholder if no certificate -->
-              <div v-else class="text-center pa-8">
-                <v-icon size="100" color="grey lighten-2"
-                  >mdi-certificate</v-icon
-                >
-                <p class="mt-4 grey--text">Sertifikat belum tersedia</p>
-              </div>
-            </v-sheet>
-          </v-col>
-        </v-row>
-
-        <!-- Student Info and Details -->
-        <v-row>
-          <v-col cols="12" class="text-center mt-8">
-            <h3 class="font-weight-bold">Detail Sertifikat Ijazah</h3>
-            <v-divider class="mt-4"></v-divider>
-          </v-col>
-        </v-row>
-
-        <!-- Student Photo and Basic Info -->
-        <v-row class="mt-4">
-          <v-col cols="12" md="4">
-            <v-card outlined class="pa-4 text-center">
-              <v-card-title class="justify-center">
-                <h4>Foto Mahasiswa</h4>
-              </v-card-title>
+        <v-btn color="blue" outlined @click="goBack" class="mb-4">
+          <v-icon left>mdi-arrow-left</v-icon>
+          Kembali
+        </v-btn>
+        <v-card class="pa-4 d-flex flex-row ga-4 justify-space-evenly">
+          <div class="pa-4 d-flex flex-column align-center ga-4">
+            <v-card-title class="text-center">
+              <h3>Sertifikat Ijazah</h3>
+            </v-card-title>
+            <!-- Photo Display -->
+            <div class="d-flex flex-column align-center justify-center">
               <div v-if="photoUrl">
-                <v-avatar size="150" class="mb-3">
+                <v-avatar rounded="0" size="150" class="mb-3">
                   <v-img
                     :src="photoUrl"
                     :alt="`Foto ${ijazah.nama}`"
                     @error="onPhotoError"
+                    cover
                   ></v-img>
                 </v-avatar>
               </div>
@@ -113,165 +51,107 @@
                   <v-icon size="80" color="grey darken-1">mdi-account</v-icon>
                 </v-avatar>
               </div>
+              <!-- Name and NIM -->
               <h3 class="font-weight-bold">{{ ijazah.nama }}</h3>
-              <p class="text-subtitle-1">{{ ijazah.nomorIndukMahasiswa }}</p>
-            </v-card>
-          </v-col>
+              <p class="text-subtitle-1">
+                NIM: {{ ijazah.nomorIndukMahasiswa }}
+              </p>
+            </div>
+            <!-- Download Button  -->
+            <div v-if="certificateUrl">
+              <v-btn class="mb-4" color="primary" @click="downloadCertificate">
+                <v-icon left>mdi-download</v-icon>
+                Download Sertifikat
+              </v-btn>
+            </div>
+          </div>
 
-          <v-col cols="12" md="8">
-            <v-row>
-              <!-- Personal Information -->
-              <v-col cols="12" sm="6">
-                <v-card outlined class="pa-4 h-100">
-                  <v-card-title class="text-uppercase font-weight-bold pb-2">
-                    <v-icon left color="primary">mdi-account-details</v-icon>
-                    Data Pribadi
-                  </v-card-title>
-                  <v-card-text>
-                    <div class="mb-2">
-                      <strong>NIK:</strong> {{ ijazah.nomorIndukKependudukan }}
-                    </div>
-                    <div class="mb-2">
-                      <strong>Tempat Lahir:</strong>
-                      {{ ijazah.tempatLahir || '-' }}
-                    </div>
-                    <div class="mb-2">
-                      <strong>Tanggal Lahir:</strong>
-                      {{ formatDate(ijazah.tanggalLahir) }}
-                    </div>
-                  </v-card-text>
-                </v-card>
-              </v-col>
+          <v-divider vertical class="mx-4"></v-divider>
 
-              <!-- Academic Information -->
-              <v-col cols="12" sm="6">
-                <v-card outlined class="pa-4 h-100">
-                  <v-card-title class="text-uppercase font-weight-bold pb-2">
-                    <v-icon left color="success">mdi-school</v-icon>
-                    Data Akademik
-                  </v-card-title>
-                  <v-card-text>
-                    <div class="mb-2">
-                      <strong>Program Studi:</strong> {{ ijazah.programStudi }}
-                    </div>
-                    <div class="mb-2">
-                      <strong>Fakultas:</strong> {{ ijazah.fakultas }}
-                    </div>
-                    <div class="mb-2">
-                      <strong>Jenis Pendidikan:</strong>
-                      {{ ijazah.jenisPendidikan }}
-                    </div>
-                    <div class="mb-2">
-                      <strong>Gelar:</strong> {{ ijazah.gelarPendidikan }}
-                    </div>
-                  </v-card-text>
-                </v-card>
-              </v-col>
-            </v-row>
-          </v-col>
-        </v-row>
+          <!-- Personal Information -->
+          <div class="pa-4 d-flex flex-column ga-4">
+            <v-card-title class="text-uppercase font-weight-bold pb-2">
+              <v-icon left color="primary">mdi-account-details</v-icon>
+              Data Pribadi
+            </v-card-title>
+            <v-card-text class="d-flex flex-column align-start ga-4">
+              <div>
+                <strong>NIK:</strong> {{ ijazah.nomorIndukKependudukan }}
+              </div>
+              <div>
+                <strong>Tempat Lahir:</strong>
+                {{ ijazah.tempatLahir || '-' }}
+              </div>
+              <div>
+                <strong>Tanggal Lahir:</strong>
+                {{ formatDate(ijazah.tanggalLahir) }}
+              </div>
+              <div>
+                <strong>Program Studi:</strong> {{ ijazah.programStudi }}
+              </div>
+              <div><strong>Fakultas:</strong> {{ ijazah.fakultas }}</div>
+              <div>
+                <strong>Jenis Pendidikan:</strong>
+                {{ ijazah.jenisPendidikan }}
+              </div>
+              <div><strong>Gelar:</strong> {{ ijazah.gelarPendidikan }}</div>
+            </v-card-text>
+          </div>
 
-        <!-- Document Information -->
-        <v-row class="mt-4">
-          <v-col cols="12" md="6">
-            <v-card outlined class="pa-4">
+          <v-divider vertical class="mx-4"></v-divider>
+
+          <div class="pa-4 d-flex flex-column ga-2">
+            <!-- Document Information -->
+            <div class="d-flex flex-column ga-4">
               <v-card-title class="text-uppercase font-weight-bold pb-2">
-                <v-icon left color="info">mdi-file-document</v-icon>
+                <v-icon left color="primary">mdi-file-document</v-icon>
                 Informasi Dokumen
               </v-card-title>
-              <v-card-text>
-                <div class="mb-2">
+              <v-card-text class="d-flex flex-column align-start ga-2">
+                <div>
                   <strong>Nomor Dokumen:</strong> {{ ijazah.nomorDokumen }}
                 </div>
-                <div class="mb-2">
+                <div>
                   <strong>Nomor Ijazah Nasional:</strong>
                   {{ ijazah.nomorIjazahNasional }}
                 </div>
-                <div class="mb-2">
+                <div>
                   <strong>Tempat Diberikan:</strong>
                   {{ ijazah.tempatIjazahDiberikan }}
                 </div>
-                <div class="mb-2">
+                <div>
                   <strong>Tanggal Diberikan:</strong>
                   {{ formatDate(ijazah.tanggalIjazahDiberikan) }}
                 </div>
               </v-card-text>
-            </v-card>
-          </v-col>
+            </div>
 
-          <v-col cols="12" md="6">
-            <v-card outlined class="pa-4">
+            <!-- Academic Timeline -->
+            <div class="d-flex flex-column ga-4">
               <v-card-title class="text-uppercase font-weight-bold pb-2">
-                <v-icon left color="warning">mdi-calendar-clock</v-icon>
+                <v-icon left color="primary">mdi-calendar-clock</v-icon>
                 Timeline Akademik
               </v-card-title>
-              <v-card-text>
-                <div class="mb-2">
+              <v-card-text class="d-flex flex-column align-start ga-2">
+                <div>
                   <strong>Tahun Diterima:</strong> {{ ijazah.tahunDiterima }}
                 </div>
-                <div class="mb-2">
+                <div>
                   <strong>Tanggal Lulus:</strong>
                   {{ formatDate(ijazah.tanggalLulus) }}
                 </div>
-                <div class="mb-2">
+                <div>
                   <strong>Akreditasi:</strong>
                   {{ ijazah.akreditasiProgramStudi }}
                 </div>
-                <div class="mb-2">
+                <div>
                   <strong>Keputusan Akreditasi:</strong>
                   {{ ijazah.keputusanAkreditasiProgramStudi }}
                 </div>
               </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
-
-        <!-- Actions -->
-        <v-row class="mt-6">
-          <v-col cols="12" class="text-center">
-            <v-btn
-              color="success"
-              large
-              @click="validateCertificate"
-              :loading="validating"
-              class="mr-4"
-            >
-              <v-icon left>mdi-shield-check</v-icon>
-              Validasi Sertifikat
-            </v-btn>
-
-            <v-btn
-              color="primary"
-              large
-              @click="verifyCertificate"
-              :loading="verifying"
-            >
-              <v-icon left>mdi-certificate-outline</v-icon>
-              Verifikasi Sertifikat
-            </v-btn>
-          </v-col>
-        </v-row>
-
-        <!-- Validation/Verification Results -->
-        <v-row v-if="validationResult || verificationResult" class="mt-4">
-          <v-col cols="12">
-            <v-alert
-              v-if="validationResult"
-              :type="validationResult.isValid ? 'success' : 'error'"
-              class="mb-4"
-            >
-              <strong>Hasil Validasi:</strong> {{ validationResult.message }}
-            </v-alert>
-
-            <v-alert
-              v-if="verificationResult"
-              :type="verificationResult.isVerified ? 'success' : 'warning'"
-            >
-              <strong>Hasil Verifikasi:</strong>
-              {{ verificationResult.message }}
-            </v-alert>
-          </v-col>
-        </v-row>
+            </div>
+          </div>
+        </v-card>
       </div>
 
       <!-- Not Found State -->
@@ -309,12 +189,6 @@ const ijazahId = computed(() => route.params.id as string)
 const ijazah = ref<Ijazah | null>(null)
 const loading = ref(false)
 const error = ref<string | null>(null)
-const validating = ref(false)
-const verifying = ref(false)
-const validationResult = ref<{ isValid: boolean; message: string } | null>(null)
-const verificationResult = ref<{ isVerified: boolean; message: string } | null>(
-  null
-)
 
 // Computed properties
 const certificateUrl = computed(() => {
@@ -365,52 +239,6 @@ const fetchIjazah = async () => {
   }
 }
 
-const validateCertificate = async () => {
-  if (!ijazah.value) return
-
-  validating.value = true
-  validationResult.value = null
-
-  try {
-    const response = await apiService.ijazah.validate(ijazah.value.ID)
-    validationResult.value = {
-      isValid: response.data?.valid || false,
-      message: response.data?.message || 'Sertifikat berhasil divalidasi',
-    }
-  } catch (err) {
-    console.error('Error validating certificate:', err)
-    validationResult.value = {
-      isValid: false,
-      message: apiHelper.getErrorMessage(err as Error),
-    }
-  } finally {
-    validating.value = false
-  }
-}
-
-const verifyCertificate = async () => {
-  if (!ijazah.value) return
-
-  verifying.value = true
-  verificationResult.value = null
-
-  try {
-    const response = await apiService.ijazah.verify(ijazah.value.ID)
-    verificationResult.value = {
-      isVerified: response.data?.verified || false,
-      message: response.data?.message || 'Sertifikat berhasil diverifikasi',
-    }
-  } catch (err) {
-    console.error('Error verifying certificate:', err)
-    verificationResult.value = {
-      isVerified: false,
-      message: apiHelper.getErrorMessage(err as Error),
-    }
-  } finally {
-    verifying.value = false
-  }
-}
-
 const downloadCertificate = async () => {
   if (!ijazah.value || !certificateUrl.value) return
 
@@ -447,10 +275,6 @@ const formatDate = (dateString: string): string => {
   } catch {
     return dateString
   }
-}
-
-const onImageError = () => {
-  console.warn('Failed to load certificate image')
 }
 
 const onPhotoError = () => {
